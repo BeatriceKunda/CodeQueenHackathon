f529efafe22a7b4d395a1874dee9be3938a28ba0
BeatriceKunda <beatrice.kunda96@gmail.com>
2020-05-19 23:19:49 +0300

    layers app for MVC architecture and installs mongoose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// ----- DATABASE -----
+mongoose
+    .connect("mongodb://localhost:27017/sb-driver-recruit", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connection Succesful 😃✅"));
 
 
 // ------- ROUTES -------
