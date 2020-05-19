be20512b8994af30d047c32c411ad83ed73b131b
BeatriceKunda <beatrice.kunda96@gmail.com>
2020-05-20 01:24:08 +0300

    adds recruiter schema

---
+++ server/models/recruiters.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Recruiter = new Schema({
+    firstName: { type: String, required: [true, "Please provide a first name"] },
+    lastName: { type: String, required: [true, "Please provide a last Name"] },
+    username: { type: String, required: [true, "Please provide a username"] },
+    password: { type: String, required: [true, "Please provide a password"], minlength: 8 },
+    passwordConfirm: { type: String, required: [true, "Please confirm your password"] }, // TODO: Add a  password validator
+    email: { type: String, required: [true, "Please provide an email"] }, // TODO: Add email validation
+    phoneNumber: { type: String, required: [true, "Please a phone Number"] }, // TODO: Add Number validation
+    drivers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Driver" }]
+});
+
+module.exports = mongoose.model("Recruiter", Recruiter);
