0f2e3bfc22522ad5763055ee9257effcf03f0ed6
BeatriceKunda <beatrice.kunda96@gmail.com>
2020-05-20 22:55:05 +0300

    adds staff controllers and connects them to the db

--- server/controllers/staff.js
+++ server/controllers/staff.js
@@ -1,21 +1,55 @@
-const getAllStaff = (req, res) => {
-    res.status(200).json({ message: "success" });
+const Staff = require("../models/staff");
+
+const getAllStaff = async (req, res) => {
+    const staff = await Staff.find({});
+    res.status(200).json({ message: "success", staff });
 }
 
-const createNewStaffMember = (req, res) => {
-    res.status(201).json({ message: "success" });
+const createNewStaffMember = async (req, res) => {
+    try {
+        const staff = await Staff.create(req.body);
+        res.status(201).json({ message: "success", staff });
+    } catch (error) {
+        console.log(error);
+        res.status(400).error(error);
+    }
 }
 
-const getSpecificStaffDetails = (req, res) => {
-    res.status(200).json({ message: "success" });
+const getSpecificStaffDetails = async (req, res) => {
+    try {
+        const staff = await Staff.findById(req.params.id);
+        res.status(200).json({ message: "success", staff });
+    }
+    catch (error) {
+        // console.log(error);
+        res.status(404).json({ message: "No Staff Member Found with That ID" })
+    }
 }
 
-const updateStaffMemberDetails = (req, res) => {
-    res.status(200).json({ message: "success" });
+const updateStaffMemberDetails = async (req, res) => {
+    try {
+        const staff = await Staff.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(200).json({ message: "successfully updated staff", staff });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: "Could not update that ID as it is not Found" });
+    }
 }
 
-const deleteStaffMember = (req, res) => {
-    res.status(204).json({ message: "success" });
+const deleteStaffMember = async (req, res) => {
+    try {
+        const staff = await Staff.findByIdAndDelete(req.params.id);
+        res.status(204).json({ message: "successfully removed staff from database" });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: "No staff found with that ID" })
+    }
 }
 
-module.exports = { getAllStaff, createNewStaffMember, getSpecificStaffDetails, updateStaffMemberDetails, deleteStaffMember }
+module.exports = {
+    getAllStaff,
+    createNewStaffMember,
+    getSpecificStaffDetails,
+    updateStaffMemberDetails,
+    deleteStaffMember
+}
--- server/models/staff.js
+++ server/models/staff.js
@@ -1,16 +1,57 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const Staff = new Schema({
     firstName: { type: String, required: [true, "Please provide a first name"] },
     lastName: { type: String, required: [true, "Please provide a last Name"] },
-    username: { type: String, required: [true, "Please provide a username"] },
+    username: { type: String, required: [true, "Please provide a username"], unique: true, lowercase: true },
     password: { type: String, required: [true, "Please provide a password"], minlength: 8 },
-    passwordConfirm: { type: String, required: [true, "Please confirm your password"] }, // TODO: Add a  password validator
-    email: { type: String, required: [true, "Please provide an email"] }, // TODO: Add email validation
-    phoneNumber: { type: String, required: [true, "Please a phone Number"] }, // TODO: Add Number validation
+    passwordConfirm: {
+        type: String,
+        required: [true, "Please confirm your password"],
+        validate: {
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: "Passwords Do not match!"
+        }
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid Email"]
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, "Please a phone Number"],
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function (num) {
+                return validator.isMobilePhone(num, "en-UG"); // Restrict to UG numbers
+            },
+            message: "Please provide a valid Ugandan Mobile Number"
+        }
+    },
     drivers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Driver" }],
     role: { type: String, enum: ['admin', 'recruiter'], default: 'recruiter' }
 });
+
+Staff.pre("save", async function (next) {
+    // hash the pasword with a cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // no need to keep the passwordConfirm field after encrypting the password
+    this.passwordConfirm = undefined;
+
+    // this ensures that you the call next function in the middleware stack
+    next();
+});
+
+
 
 module.exports = mongoose.model("staff", Staff);
