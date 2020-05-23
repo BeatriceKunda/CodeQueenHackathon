3ab26e7f7aecd295fa6105b3f26c3adf62cb1b99
BeatriceKunda <beatrice.kunda96@gmail.com>
2020-05-23 12:21:53 +0300

    adds validation to driver model fields
    ... also removes duplicate registeredOn field

--- server/models/drivers.js
+++ server/models/drivers.js
@@ -6,22 +6,46 @@ const Driver = new Schema({
     stage: { type: String, required: true },
     natIdNumber: { type: String, required: true },
     age: { type: Number, required: true },
-    phoneNumber: { type: String, required: true },
-    recruitmentDate: { type: Date, required: true },
+    phoneNumber: {
+        type: String,
+        required: [true, "Please a phone Number"],
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function (num) {
+                return validator.isMobilePhone(num, "en-UG"); // Restrict to UG numbers
+            },
+            message: "Please provide a valid Ugandan Mobile Number"
+        }
+    },
+    recruitedOn: { type: Date, required: true, default: Date.now },
     recruitmentAddr: { type: String, required: true },
     referredBy: { type: String },
     nextOfKin: { type: String, required: true },
-    nextOfKinContact: { type: String, required: true },
+    nextOfKinContact: {
+        type: String,
+        required: [true, "Please a phone Number"],
+        unique: true,
+        trim: true,
+        validate: {
+            validator: function (num) {
+                return validator.isMobilePhone(num, "en-UG"); // Restrict to UG numbers
+            },
+            message: "Please provide a valid Ugandan Mobile Number"
+        }
+    },
     photo: String,
-    email: String,
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, "Please provide a valid Email"]
+    },
     onBoardingStage: {
         type: String,
         enum: ["registered", "inTraining", "readyForActivation"],
         default: "registered"
-    },
-    registeredOn: {
-        type: Date,
-        default: Date.now
     }
 });
 
