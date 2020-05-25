b61ecd9f7c4abaf317d70fa6acdb0d94e3ce968d
BeatriceKunda <beatrice.kunda96@gmail.com>
2020-05-25 22:03:13 +0300

    adds logout functionality and middleware to check loggedIn status

--- server/routes/viewRoutes.js
+++ server/routes/viewRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const viewsController = require("../controllers/views");
 const router = express.Router();
 
-router.get('/dashboard', viewsController.dashboard);
+router.get('/dashboard', viewsController.isLoggedIn, viewsController.dashboard);
+router.get('/', viewsController.login);
 router.get('/login', viewsController.login);
-router.get('/register-driver', viewsController.registerDriver);
+router.get('/register-driver', viewsController.isLoggedIn, viewsController.registerDriver);
 
 
 
